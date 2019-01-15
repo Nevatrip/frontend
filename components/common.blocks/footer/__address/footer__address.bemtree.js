a3a4756a5d26cc6d629d3e5e6bb4874fb9e3c47a
kizoso <kizoso@bk.ru>
2019-01-15 15:47:20 +0300

    spaces

--- components/common.blocks/footer/__address/footer__address.bemtree.js
+++ components/common.blocks/footer/__address/footer__address.bemtree.js
@@ -1,13 +1,13 @@
 block('footer').elem('address')(
-    replace()((node, ctx) => {
-        return {
-            block: 'address',
-            mix: { block: node.block, elem : node.elem },
-            content: [
-                {
-                    elem: 'social'
-                }
-            ]
+  replace()((node, ctx) => {
+    return {
+      block: 'address',
+      mix: {block: node.block, elem: node.elem},
+      content: [
+        {
+          elem: 'social'
         }
-    }),
+      ]
+    }
+  }),
 );
