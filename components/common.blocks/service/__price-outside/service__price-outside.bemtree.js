536148b20368c9b694581b03fee2341a18978f3a
kizoso <kizoso@bk.ru>
2019-01-22 15:43:30 +0300

    fix service__price-outside

--- components/common.blocks/service/__price-outside/service__price-outside.bemtree.js
+++ components/common.blocks/service/__price-outside/service__price-outside.bemtree.js
@@ -1,7 +1,8 @@
 block('service').elem('price-outside')(
   content()((node, ctx) => {
-    return {
-      html: ctx.content + '&nbsp;₽ на&nbsp;причале'
+    return ctx.content && {
+      // html: ctx.content + '&nbsp;₽ на&nbsp;причале'
+      html:  ctx.content.match(/\d{2,6}/) ? ctx.content + '&nbsp;₽ на&nbsp;причале' : 'На&nbsp;причале&nbsp;' + ctx.content,
     }
   })
 );
