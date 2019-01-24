2b673c5f63d43d42f994de310c9580b087896bce
kizoso <kizoso@bk.ru>
2019-01-24 12:09:51 +0300

    set hard values to the service_price_info

--- components/common.blocks/service/__price-info/service__price-info.bemtree.js
+++ components/common.blocks/service/__price-info/service__price-info.bemtree.js
@@ -7,6 +7,13 @@ block('service').elem('price-info')(
       if (infoArr[i].startsWith('$')){
         var info = infoArr[i].slice(1, this.length);
         switch (info) {
+          case 'иностранный':
+            info = {
+              block: 'popup-tip',
+              title: 'иностранный',
+              text: 'Не резиденты РФ'
+            }
+            break;
           case 'льготный':
             info = {
               block: 'popup-tip',
@@ -19,6 +26,20 @@ block('service').elem('price-info')(
               block: 'popup-tip',
               title: 'детский',
               text: 'Дети 3-12 лет или 5-12 лет (см. конкр.экск.). До 3 или 5 - бесплатно без представления отдельного места. Необходимо предъявить подтверждающие документы в кассе.'
+            }
+            break;
+          case 'детский3':
+            info = {
+              block: 'popup-tip',
+              title: 'детский',
+              text: 'Дети 3-12 лет. До 3 - бесплатно без представления отдельного места. Необходимо предъявить подтверждающие документы в кассе.'
+            }
+            break;
+          case 'детский5':
+            info = {
+              block: 'popup-tip',
+              title: 'детский',
+              text: 'Дети 5-12 лет. До 5 - бесплатно без представления отдельного места. Необходимо предъявить подтверждающие документы в кассе.'
             }
             break;
         }
