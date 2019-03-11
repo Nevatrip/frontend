7fc3e4a2ee76fcc95e0f62c9eb077524b34ca6dd
Realetive <realetive@yandex.ru>
2019-03-11 20:23:43 +0300

    Add redirect to default project

--- server/router.js
+++ server/router.js
@@ -12,6 +12,11 @@ const error = require( './routes/error' );
 
 const router = new UniversalRouter(
   {
+    path: '/',
+    name: 'default',
+    action: () => ({ redirect: '/nevatrip/ru' })
+  },
+  {
     path: '/:project/:lang',
     name: 'root',
     children: [
