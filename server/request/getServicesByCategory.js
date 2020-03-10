c7019323e80c3ea6f9070563f5a8a898d2b2b04f
Olga Bolotova <olbolot@gmail.com>
2020-03-10 12:11:04 +0300

    Add sort for tours

--- server/request/getServicesByCategory.js
+++ server/request/getServicesByCategory.js
@@ -5,6 +5,33 @@ const query = ( categoryName, lang ) => `*[_type == "tour"&&category._ref in *[t
 "category": category->}`;
 const params = {};
 
+const tourSortByPriority = services => {
+  const sortedArray = [];
+
+  for( let i = 0; i < services.length; i++ ) {
+    if( services[i].tourPriority ) {
+      sortedArray.push( services[i] );
+      services.splice( i, 1 );
+    }
+  }
+
+  sortedArray.sort( ( a, b ) => {
+    if( a.tourPriority < b.tourPriority) {
+      return -1;
+    } else if( a.tourPriority > b.tourPriority ) {
+      return 1;
+    }
+
+    return 0;
+  } );
+
+  services = [...sortedArray, ...services];
+
+  return services;
+};
+
 module.exports = async( project, lang, categoryName ) => await client( project )
   .fetch( query( categoryName, lang ), params )
-  .then( services => services );
+  .then( services => {
+    return tourSortByPriority( services );
+  } );
