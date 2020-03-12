68903bcdbe55229d9e6047bebe35b7212c3af0b5
Olga Bolotova <olbolot@gmail.com>
2020-03-12 15:06:15 +0300

    Fix footer_footer-nav

--- components/common.blocks/footer/__footer-nav/footer__footer-nav.bemtree.js
+++ components/common.blocks/footer/__footer-nav/footer__footer-nav.bemtree.js
@@ -2,15 +2,15 @@ block( 'footer' ).elem( 'footer-nav' )(
   content()( ( node, ctx ) => [
     {
       elem: 'nav-list',
-      content: (( ctx.navFooter || {} )[0] || {} ).map( item => ( {
+      content: (( ctx.navFooter || {} )[0] || {} ).map( item => ( item.alias && {
         elem: 'nav-item',
         content: {
           mix: { block: 'footer', elem: 'nav-link' },
           block: 'link',
           content: ( item || {} ).title || '',
-          to: 'servicesByCategory',
+          to: 'article',
           params: {
-            category: item.alias
+            article: item.alias
           }
         }
       } ) )
