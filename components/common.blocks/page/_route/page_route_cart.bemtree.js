2710eeacbf8910bb7f2ae7a786c48d828b50e0c7
kizoso <kizoso@bk.ru>
2020-03-20 12:00:46 +0300

    translation prohibition

--- components/common.blocks/page/_route/page_route_cart.bemtree.js
+++ components/common.blocks/page/_route/page_route_cart.bemtree.js
@@ -1,13 +1,20 @@
 block( 'page' ).mod( 'route', 'cart' )(
   mods()( ( { data: { session, params: { lang } } } ) => [
     {
+      tag: 'meta',
+      attrs: {
+        name: 'google',
+        content: 'notranslate'
+      }
+    },
+    {
       block: 'cart',
       attrs: {
         id: 'root',
         lang,
-        translate: 'no',
         'data-session': session
-      }
+      },
+      mix: { block: 'notranslate' }
     }
   ] ),
 );
