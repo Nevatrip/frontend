629d4f5d7eaacb3fd96704bdcb698fdd08891354
Olga Bolotova <olbolot@gmail.com>
2020-03-20 17:24:13 +0300

    Sort tours by collections, tags and on home page. Add .env in .gitignore

--- server/request/getServices.js
+++ server/request/getServices.js
@@ -5,6 +5,34 @@ const query = `*[_type == "tour"&&!("deleted" in status)&&!("hidden" in status)]
 "category": category->}`;
 const params = {};
 
+const tourSortByPriority = services => {
+  const sortedArray = [];
+
+  for( let i = 0; i < services.length; i++ ) {
+    if( services[i].tourPriority ) {
+      sortedArray.push( services[i] );
+      services.splice( i, 1, {} );
+    }
+  }
+
+  sortedArray.sort( ( a, b ) => {
+    if( a.tourPriority < b.tourPriority) {
+      return -1;
+    } else if( a.tourPriority > b.tourPriority ) {
+      return 1;
+    }
+
+    return 0;
+  } );
+
+  services = [
+    ...sortedArray,
+    ...services.filter( item => Object.keys( item ).length > 0 )
+  ];
+
+  return services;
+};
+
 module.exports = async( project, lang ) => await client( project, lang )
   .fetch( query, params )
-  .then( tours => tours );
+  .then( tours => tourSortByPriority( tours ) );
--- server/request/getServicesByTags.js
+++ server/request/getServicesByTags.js
@@ -35,6 +35,34 @@ const query = tags => {
 };
 const params = {};
 
+const tourSortByPriority = services => {
+  const sortedArray = [];
+
+  for( let i = 0; i < services.length; i++ ) {
+    if( services[i].tourPriority ) {
+      sortedArray.push( services[i] );
+      services.splice( i, 1, {} );
+    }
+  }
+
+  sortedArray.sort( ( a, b ) => {
+    if( a.tourPriority < b.tourPriority) {
+      return -1;
+    } else if( a.tourPriority > b.tourPriority ) {
+      return 1;
+    }
+
+    return 0;
+  } );
+
+  services = [
+    ...sortedArray,
+    ...services.filter( item => Object.keys( item ).length > 0 )
+  ];
+
+  return services;
+};
+
 module.exports = async( project, lang, tags ) => await client( project, lang, tags )
   .fetch( query( tags ), params )
-  .then( services => services );
+  .then( services => tourSortByPriority( services ) );
