324f07e0553c6bb2b7e7d37b2fd84b578be5870c
Olga Bolotova <olbolot@gmail.com>
2020-04-01 16:10:42 +0300

    Add error page in first level url

--- server/request/getServiceCollectionByCollectionAlias.js
+++ server/request/getServiceCollectionByCollectionAlias.js
@@ -4,37 +4,39 @@ const query = ( collectionName, lang ) => `*[_type=="settingServicesCollections"
 const params = {};
 
 const tourSortByPriority = services => {
-  const sortedArray = [];
+  if ( services ) {
+    const sortedArray = [];
 
-  for( let i = 0; i < services.length; i++ ) {
-    if( services[i].tourPriority ) {
-      sortedArray.push( services[i] );
-      services.splice( i, 1, {} );
+    for (let i = 0; i < services.length; i++) {
+      if (services[i].tourPriority) {
+        sortedArray.push(services[i]);
+        services.splice(i, 1, {});
+      }
     }
+
+    sortedArray.sort((a, b) => {
+      if (a.tourPriority < b.tourPriority) {
+        return -1;
+      } else if (a.tourPriority > b.tourPriority) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    services = [
+      ...sortedArray,
+      ...services.filter(item => Object.keys(item).length > 0)
+    ];
+
+    return services;
   }
-
-  sortedArray.sort( ( a, b ) => {
-    if( a.tourPriority < b.tourPriority) {
-      return -1;
-    } else if( a.tourPriority > b.tourPriority ) {
-      return 1;
-    }
-
-    return 0;
-  } );
-
-  services = [
-    ...sortedArray,
-    ...services.filter( item => Object.keys( item ).length > 0 )
-  ];
-
-  return services;
 };
 
 module.exports = async( collectionName, lang, project ) => await client( project, lang )
   .fetch( query( collectionName, lang ), params )
   .then( collection => {
-    collection[0].services = tourSortByPriority( collection[0].services );
+    ( collection[0] || {} ).services = tourSortByPriority( ( collection[0]|| {} ).services );
 
     return collection[0];
   } );
